96daa8dc2acadc4e7804e589ff3f51750d713e22
AkashParmar7300 <akashparmar7300@gmail.com>
2026-04-06 13:44:48 +0530

    read big file through streams or chunks of data

--- app.js
+++ app.js
@@ -1,7 +1,17 @@
-import fs from 'fs/promises'
+import fs from 'fs'
 
+ /*console.time()
 const a =await  fs.readFile("c:\\Users\\dell\\OneDrive\\Desktop\\ff.mp4 - Shortcut.lnk")
 
-fs.writeFile( "base.mp4"  ,a)
 //console.log(a.toString())
-console.log(a.byteLength)
+console.log(a.byteLength)c:\Users\dell\OneDrive\Desktop\fff.mp4
+console.timeEnd()*/
+console.time()
+const read =fs.createReadStream("c:\\Users\\dell\\OneDrive\\Desktop\\fff.mp4",{highWaterMark : 100*1024*1024})
+
+read.on('data',(chunkbuffer)=>{
+  console.log(chunkbuffer)
+  console.log(chunkbuffer.byteLength)
+
+})
+console.timeEnd()
